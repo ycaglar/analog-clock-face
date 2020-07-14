c797c67cd38797e0cbd5c6a97e0abac207165b3c
Caglar <caalar@msn.com>
2020-07-14 16:01:15 -0500

    Initial Commit

---
+++ Analog Clock Face/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Analog Clock Face
+//
+//  Created by Caglar on 7/14/20.
+//  Copyright © 2020 Caglar. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
